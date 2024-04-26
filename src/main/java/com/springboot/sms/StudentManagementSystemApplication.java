1e26895e078947347c2ac49d4e261cfa1d60bc4f
MiryalkarManojKumar <miryalkarmanoj8543@gmail.com>
2024-04-26 17:52:15 +0530

    Student Management System commit

---
+++ src/main/java/com/springboot/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,33 @@
+package com.springboot.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.springboot.sms.entity.Student;
+import com.springboot.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		/*
+		 * Student student1 = new Student("Manoj","Kumar","manoj@gmail.com");
+		 * studentRepository.save(student1); Student student2 = new
+		 * Student("Raam","chandra","raam@gmail.com"); studentRepository.save(student2);
+		 * Student student3 = new Student("Lakshman","seshu","seshu@gmail.com");
+		 * studentRepository.save(student3);
+		 */
+		
+	}
+
+}
